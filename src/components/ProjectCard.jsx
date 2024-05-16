1099f7730e74231e4c978970c8e8dd548424c8c0
elfadili-ae <elfadili.ae@gmail.com>
2024-05-16 13:21:43 +0100

    added link to cards

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 
 import Image from 'next/image'
+import Link from 'next/link'
 
 const ProjectCard = ({ pic1, pic2, name1, name2, flip }) => {
     return (
         <div className={`flex gap-8 flex-col ${flip ? 'md:flex-row-reverse' : 'md:flex-row'} justify-around items-center`}>
-            <div className='flex w-fit h-fit flex-col items-between justify-center gap-4 bg-slate-800 p-6 border-2 border-purple-200'>
+            <Link href='/portfolio-details' className='flex w-fit h-fit flex-col items-between justify-center gap-4 bg-slate-800 p-6 border-2 border-purple-200 hover:border-purple-600'>
                 <div className='max-w-72 md:aspect-square object-cover overflow-hidden'>
                     <Image src={pic1} alt="project photo" className='w-full aspect-auto object-cover' />
                 </div>
@@ -13,8 +14,8 @@ const ProjectCard = ({ pic1, pic2, name1, name2, flip }) => {
                     <p>Modelling - 2024</p>
                     <h3 className='text-xl font-bold'>{name1}</h3>
                 </footer>
-            </div>
-            <div className='flex w-full md:max-w-[420px] md:aspect-square flex-col items-center justify-center gap-4 bg-slate-800 p-6 border-2 border-purple-200'>
+            </Link>
+            <Link href='/portfolio-details' className='flex w-full md:max-w-[420px] md:aspect-square flex-col items-center justify-center gap-4 bg-slate-800 p-6 border-2 border-purple-200 hover:border-purple-600'>
                 <div className='w-full aspect-auto object-fit overflow-hidden'>
                     <Image src={pic2} alt="project photo" className='w-full object-cover' />
                 </div>
@@ -22,7 +23,7 @@ const ProjectCard = ({ pic1, pic2, name1, name2, flip }) => {
                     <p>Modelling - 2024</p>
                     <h3 className='text-xl font-bold'>{name2}</h3>
                 </footer>
-            </div>
+            </Link>
         </div>
     )
 }
